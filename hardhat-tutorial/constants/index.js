368c4c4b06cea80f4e747aedd256001c7b03b498
Shiva-Sai-ssb <shivas.bojjalwar@gmail.com>
2023-03-21 20:44:35 +0530

    Modified

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xf7d2D1072fEAa334b56377fB723bBe9083d64fd5";
+const WHITELIST_CONTRACT_ADDRESS = "0xBdbe08071Cd4013c7d8DaF37c192Ea90a00729F6";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-ten-iota.vercel.app/api/";
 
